793e225b0868d9dad7e3bfe741db997218c89b69
FadlAlmatari <fathlvvee@gmail.com>
2024-08-12 21:47:57 +0300

    rr

--- resources/views/home/indxe.blade.php
+++ resources/views/home/indxe.blade.php
@@ -11,7 +11,7 @@
                     <h3 class="text-base font-normal text-gray-500">New products this week</h3>
                 </div>
                 <div class="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
-                    14.20%
+                    14.6%
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z"
