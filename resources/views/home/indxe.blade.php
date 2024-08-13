36c786b602221974c9598d1aa26703bd212648d0
jamalAli-711 <fathlvvrr@gmail.com>
2024-08-13 01:27:19 -0400

    iiii

--- resources/views/home/indxe.blade.php
+++ resources/views/home/indxe.blade.php
@@ -2,11 +2,6 @@
 
 @section('conm')
 <div class=" ">
-<<<<<<< HEAD
-{{-- ________________________________jamal__________fjj_____________________ --}}
-=======
-{{-- ________________________________jamal__________fjj______________________ --}}
->>>>>>> 74e9be434de2ba56b3cd31ab7cc8a8424053f91c
     <div class="mt-4 w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 p-20">
         <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
             <div class="flex items-center">
@@ -15,7 +10,6 @@
                     <h3 class="text-base font-normald text-gray-500">New products this week</h3>
                 </div>
                 <div class="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
-                    14.20%
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z"
@@ -31,7 +25,6 @@
                     <h3 class="text-base font-normal text-gray-500">Visitors this week</h3>
                 </div>
                 <div class="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
-                    35.9%
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z"
@@ -47,7 +40,6 @@
                     <h3 class="text-base font-normal text-gray-500">User signups this week</h3>
                 </div>
                 <div class="ml-5 w-0 flex items-center justify-end flex-1 text-red-500 text-base font-bold">
-             jamalAli
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z"
