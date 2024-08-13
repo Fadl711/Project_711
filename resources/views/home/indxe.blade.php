74e9be434de2ba56b3cd31ab7cc8a8424053f91c
jamalAli-711 <fathlvvrr@gmail.com>
2024-08-12 21:36:58 -0400

    gamal

--- resources/views/home/indxe.blade.php
+++ resources/views/home/indxe.blade.php
@@ -2,13 +2,13 @@
 
 @section('conm')
 <div class=" ">
-{{-- ________________________________jamal________________________________ --}}
+{{-- ________________________________jamal__________fjj______________________ --}}
     <div class="mt-4 w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 p-20">
         <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
             <div class="flex items-center">
                 <div class="flex-shrink-0">
                     <span class="text-2xl sm:text-3xl leading-none font-bold text-gray-900">2,340</span>
-                    <h3 class="text-base font-normal text-gray-500">New products this week</h3>
+                    <h3 class="text-base font-normald text-gray-500">New products this week</h3>
                 </div>
                 <div class="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
                     14.6%
