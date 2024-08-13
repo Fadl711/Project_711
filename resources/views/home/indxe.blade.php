b04641f621c322ed1900cf202d03d3c198ea08a7
jamalAli-711 <fathlvvrr@gmail.com>
2024-08-12 21:57:59 -0400

    yes

--- resources/views/home/indxe.blade.php
+++ resources/views/home/indxe.blade.php
@@ -47,7 +47,7 @@
                     <h3 class="text-base font-normal text-gray-500">User signups this week</h3>
                 </div>
                 <div class="ml-5 w-0 flex items-center justify-end flex-1 text-red-500 text-base font-bold">
-                    -fadl711%
+             jamalAli
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z"
