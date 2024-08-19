ae88ddd4e9e1fffe6c313bd5592259870adf2c95
Fadl711 <fathlvvee@gmail.com>
2024-08-20 01:28:47 +0300

    fadl

--- resources/views/includes/swip.blade.php
+++ resources/views/includes/swip.blade.php
@@ -1,15 +1,4 @@
 
-<div  class=" flex   ">
-    <div class=" max-sm:w-15   ">
-
-        <div class="w-2/2 min-h-screen text-white sticky top-0 p-4 bg-amber-400  border-r border-gray-100 ">
-            <ul class="list-none ">
-                <li class="mb-2">
-                    <a class="NavTagA" href="{{route('home.indxe')}}">
-                        <svg class="w-6 h-6 stroke-current " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                      stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
 <div class="max-w-72">
 <div class="max-sm:w-15">
     <div class="w-2/2 min-h-screen text-white sticky top-0 p-4 bg-[#6A64F1] border-b border-gray-100">
