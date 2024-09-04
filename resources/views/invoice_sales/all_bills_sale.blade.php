9085fb3d714152065f0d87cc0f81ed6fffee0e5f
Fadl711 <fathlvvee@gmail.com>
2024-09-04 21:16:47 +0300

    ino

--- resources/views/invoice_sales/all_bills_sale.blade.php
+++ resources/views/invoice_sales/all_bills_sale.blade.php
@@ -43,8 +43,8 @@
                     <input name="horizontal-list-radio-passpot" class="inputSale " id="" type="date" placeholder=""/>
                 </div>
             </li>
-        </ul> 
-    </form>  
+        </ul>
+    </form>
 <div class="flex  gap-4 justify-center items-center  ">
     <div class="">
 
@@ -71,7 +71,7 @@
                             <td colspan="1" class=" text-right  p-1  bg-white text-black  ">
                                 <div class="flex items-center ">
                                     <div class="text-indigo-700 text-sm ">اسم العميل : <label class="text-black" for="">     جمال علي احمد</label> </div>
-                                </div>                   
+                                </div>
                                 <div class="text-indigo-700  text-sm"> التلفون: <label class="text-black" for="">775454554 </label></div>
                                 <div class="text-sm text-indigo-700">الرقم التعريفي :  <label class="text-black" for="">15 </label></div>
                             </td>
@@ -101,7 +101,7 @@
                         <td class=" text-center text-indigo-700 "> <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="   focus:outline-none " type="button">
                           عرض مبيعات  الفاتورة   </button>
                        </td>
-                    </tr> 
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -112,25 +112,35 @@
 </div>
 
 {{-- alert --}}
-<div id="crud-modal" tabindex="-1" aria-hidden="true" class=" bg-black bg-opacity-50  hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0  h-full">
-    <div class="relative p-4 w-full max-w-xl max-h-full">
+
+<div  id="crud-modal"    tabindex="-1" aria-hidden="true" class=" bg-black bg-opacity-50  hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0  h-full">
+    <div class="  relative p-2 w-full max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div class=" bg-white rounded-lg shadow dark:bg-gray-700 container" x-data="invoices()" >
             <!-- Modal header -->
-            <div class="flex items-center justify-between p-1 md:p-5 border-b rounded-t dark:border-[#f8c21f]">
-               
+            <div class="flex items-center justify-between p-1 md:p-5 border-b rounded-t dark:border-gray-600">
+
                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                     </svg>
                     <span class="sr-only">Close modal</span>
                 </button>
+
+
             </div>
+
             <!-- Modal body -->
-            @include('invoice_sales.bills_sale_show')
+            <div  x-refs="printTemplate"  id="js-print-template" >
+                @include('invoice_sales.bills_sale_show')
+
+        </div>
         </div>
     </div>
 </div>
 {{-- alert --}}
 <br>
+<br>
+
+
 
