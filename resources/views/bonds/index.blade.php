b8afa904c348394ac0b8ac818a7c33328b9ff9a3
jamalAli-711 <fathlvvrr@gmail.com>
2024-09-10 04:33:55 -0400

    bonds2

--- resources/views/bonds/index.blade.php
+++ resources/views/bonds/index.blade.php
@@ -1,21 +1,93 @@
 @extends('layout')
 @section('conm')
-<div class=" bg-gray-50 ">
 
-<nav class="bg-white text-black py-1   rounded shadow-md flex items-center justify-between ">
-    <a class="font-bold  tracking-tight px-2" href="#" >محلاتي</a>
-    <div class="flex items-center">
-        <button onclick="NewAccount()" type="button" class="text-sm px-4 py-2 leading-none rounded-md hover:bg-gray-100" >اضافة حساب</button>
-        <button onclick="AccountTree()" type="button" class="text-sm px-4 py-2 leading-none rounded-md hover:bg-gray-100" >شجرة الحسابات</button>
-        <button onclick="FinancialAccounts()" type="button" class="text-sm px-4 py-2 leading-none rounded-md hover:bg-gray-100" >  مراجعة الحسابات </button>
-       {{-- <form action="{{route('accounts.balancing')}}" method="GET"> --}}
-            <button onclick="AccountBalancing()"  id="Accountbalancing"  type="submit"  class="text-sm px-4 py-2 leading-none rounded-md hover:bg-gray-100" > ترصيد الحسابات</button>
-        {{-- </form>  --}}
-        {{-- <a href="{{route('accounts.balancing')}}" onclick="AccountBalancing()"  class="text-sm px-4 py-2 leading-none rounded-full hover:bg-gray-700" > ترصيد الحسابات</a> --}}
+
+        <ul class="grid grid-cols-4 text-right container relative shadow-md px-2 py-2  bg-white">
+            <li class="  rounded ">
+                <button onclick="NewReceiptBonds()" type="button" class="text-sm  py-2     leading-none rounded-md hover:bg-gray-100" > سند قبض جديد</button>
+            </li>
+            <li class="   rounded ">
+                <button onclick="AllReceiptBonds()" type="button" class="text-sm py-2  leading-none rounded-md hover:bg-gray-100" > المقبوضات</button>
+
+            </li>
+            <li class="  rounded "> 
+                <button onclick="NewExchangeBonds()" type="button" class="text-sm py-2  leading-none rounded-md hover:bg-gray-100" > سند صرف جديد </button>
+
+            </li>
+            <li class="    rounded ">  
+                <button onclick="AccountBalancing()" id="Accountbalancing" type="submit"  class="text-sm py-2  rounded-md hover:bg-gray-100" >  المدفوعات</button>
+            </li>
+
+        </ul>
+       
+
+        <div class=" bg-slate-50  ">
+
+
+    <div class="  p-2 mt-2 " id="newExchangeBonds " style="display:">
+        @include('bonds.exchange_bonds.index')
     </div>
-</nav>
-    <div class="  p-2 mt-2" id="new_account" style="display:">
+    <div class="   p-2 mt-2" id="newReceiptBonds " style="display:none">
         @include('bonds.receipt_bonds.index')
     </div>
+
+    <div class="   p-2 " id="AllReceipt_bonds" style="display:none">
+
+       @include('bonds.receipt_bonds.all_receipt_bonds')
+    </div>
 </div>
+
+<script>
+    
+// function NewExchangeBonds(){   
+ 
+//     var allreceiptbonds= document.getElementById('AllReceipt_bonds');
+//         var ExchangeBonds= document.getElementById('newExchangeBonds');
+//         var newreceipt_bonds= document.getElementById('newReceiptBonds'); 
+
+//     if(ExchangeBonds.style.display=="none"){
+//         ExchangeBonds.style.display="block";
+//         allreceipt_bonds.style.display="none";
+//         exchange_bonds.style.display="none";
+
+//     }else  if(ExchangeBonds.style.display=="none")
+//     {
+//         ExchangeBonds.style.display="block";
+//         newreceipt_bonds.style.display="none";
+//         allreceipt_bonds.style.display="none";
+
+//     }
+
+//     }
+    
+// function NewReceiptBonds(){   
+   
+//     var allreceiptbonds= document.getElementById('AllReceipt_bonds');
+//         var exchange_bonds= document.getElementById('exchange_bonds');
+//         var newreceipt_bonds= document.getElementById('newreceipt_bonds'); 
+
+//     if(allreceiptbonds.style.display=="none"){
+//         allreceiptbonds.style.display="block";
+//         newreceipt_bonds.style.display="none";
+//         exchange_bonds.style.display="none";
+
+//     }
+
+//     }
+
+//     function AllReceiptBonds(){
+    
+//         var allreceiptbonds= document.getElementById('AllReceipt_bonds');
+//         var exchange_bonds= document.getElementById('exchange_bonds');
+//         var newreceipt_bonds= document.getElementById('newreceipt_bonds'); 
+
+//     if(allreceiptbonds.style.display=="none"){
+//         allreceiptbonds.style.display="block";
+//         newreceipt_bonds.style.display="none";
+//         exchange_bonds.style.display="none";
+
+//     }
+    
+//     }
+</script>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -29,7 +29,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased a">
-        <div class="flex max-h-screen overflow-hidden ">
+        <div class="flex max-h-screen    ">
 
 
             @include('includes.swip')
