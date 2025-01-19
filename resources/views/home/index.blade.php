0d1d1d560c8bb826e73f834fd42b1d1b86f4d405
Fadl711 <fathlvvee@gmail.com>
2025-01-19 03:20:26 +0300

    gsdgd

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,11 +1,27 @@
 @extends('layout')
 
 @section('conm')
+<style>
+    @keyframes wave {
+    0% { transform: rotate(0deg); }
+    15% { transform: rotate(14deg); }
+    30% { transform: rotate(-8deg); }
+    40% { transform: rotate(14deg); }
+    50% { transform: rotate(-4deg); }
+    60% { transform: rotate(10deg); }
+    100% { transform: rotate(0deg); }
+  }
 
-    <div class="bg-blue-50 border border-blue-300 rounded-md p-4 mt-4 text-center">
+  .animate-wave {
+    animation: wave 2s infinite;
+    transform-origin: bottom center;
+  }
+</style>
+
+    <div class="bg-blue-100 border border-blue-300 rounded-md p-4 mt-4 text-center">
         <div class="flex justify-center items-center mt-6">
+            <p class="ml-3 text-2xl font-semibold"> مرحبًا بك {{auth()->user()->name}} </p>
             <span class="text-4xl animate-wave">👋</span>
-            <p class="ml-3 text-xl font-semibold">{{auth()->user()->name}} مرحبًا بك </p>
         </div>
     </div>
 
