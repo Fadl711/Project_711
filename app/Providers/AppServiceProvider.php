12e228fa2c9707ff3553a1ad4231a91b8094dccd
Fadl711 <fathlvvee@gmail.com>
2025-02-21 01:22:29 +0300

    fasff

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,6 @@ use App\Models\User;
 use App\Models\UserPermission;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -42,202 +41,159 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        try {
-            if (Schema::hasTable('users')) {
-                $users = DB::table('users')->get();
-            }
+        $users = DB::table('users')->get();
 
-            // $dataDeportattons=[
-            //     ['Deportatton'=> (Deportatton::FINANCAL_CENTER_LIST ),'id'=>(IntOrderStatus::FINANCAL_CENTER_LIST )],
-            //     ['Deportatton'=> (Deportatton::INCOME_STATEMENT),'id'=>(IntOrderStatus::INCOME_STATEMENT)],
-            // ];
-            // View::share('dataDeportattons', $dataDeportattons);
+        $dataDeportattons=[
+            ['Deportatton'=> (Deportatton::FINANCAL_CENTER_LIST ),'id'=>(IntOrderStatus::FINANCAL_CENTER_LIST )],
+            ['Deportatton'=> (Deportatton::INCOME_STATEMENT),'id'=>(IntOrderStatus::INCOME_STATEMENT)],
+ ];
+ if (DB::connection()->getPdo()) {
+    // منطق الاستعلام هنا
+}
 
-            $TypesAccountName=[
-                ['TypesAccountName' => Deportatton::CURRENT_ASSETS, 'id' => AccountType::CURRENT_ASSETS],
-                ['TypesAccountName' => Deportatton::FIXED_ASSETS, 'id' => AccountType::FIXED_ASSETS],
-               ['TypesAccountName' => Deportatton::LIABILITIES_OPPONENTS, 'id' => AccountType::LIABILITIES_OPPONENTS],
-               ['TypesAccountName' => Deportatton::EXPENSES, 'id' => AccountType::EXPENSES],
-               ['TypesAccountName' => Deportatton::REVENUE, 'id' => AccountType::REVENUE],
+        $TypesAccountName=[
+            ['TypesAccountName' => Deportatton::CURRENT_ASSETS, 'id' => AccountType::CURRENT_ASSETS],
+            ['TypesAccountName' => Deportatton::FIXED_ASSETS, 'id' => AccountType::FIXED_ASSETS],
+           ['TypesAccountName' => Deportatton::LIABILITIES_OPPONENTS, 'id' => AccountType::LIABILITIES_OPPONENTS],
+           ['TypesAccountName' => Deportatton::EXPENSES, 'id' => AccountType::EXPENSES],
+           ['TypesAccountName' => Deportatton::REVENUE, 'id' => AccountType::REVENUE],
 
-            ];
-            View::share('TypesAccountName', $TypesAccountName);
-
-            if (Schema::hasTable('main_accounts')) {
-                $mainAccount_Warehouse=MainAccount::where('AccountClass',AccountClass::STORE->value)->first();
-                if ($mainAccount_Warehouse)
-                {
-                    $subAccount=SubAccount::where('Main_id',$mainAccount_Warehouse->main_account_id)->get();
-                }
-                View::share('Warehouse', $subAccount);
-            }
-
-            if (Schema::hasTable('products')) {
-                $products = Product::all();
-                if(isset($products)){
-                    View::share([
-                        'products' => $products,
+        ];
+        $mainAccount_Warehouse=MainAccount::where('AccountClass',AccountClass::STORE->value)->first();
+        if ($mainAccount_Warehouse)
+        {
+            $subAccount=SubAccount::where('Main_id',$mainAccount_Warehouse->main_account_id)->get();
+        }
+        $products = Product::all();
+        if(isset($products)){
+            View::share([
+                'products' => $products,
 
 
-                    ]);
-                }
-            }
+            ]);
+        }
+        $mainAccount_Supplier=MainAccount::where('AccountClass',AccountClass::SUPPLIER->value)->first();
 
-            if (Schema::hasTable('main_accounts')) {
-                $mainAccount_Supplier=MainAccount::where('AccountClass',AccountClass::SUPPLIER->value)->first();
+            $subAccountSupplierid=SubAccount::where('AccountClass',AccountClass::SUPPLIER->value)->get();
 
-                $subAccountSupplierid=SubAccount::where('AccountClass',AccountClass::SUPPLIER->value)->get();
-
-                if(isset($subAccountSupplierid)){
-                    View::share([
-                        'subAccountSupplierid' => $subAccountSupplierid,
+        if(isset($subAccountSupplierid)){
+            View::share([
+                'subAccountSupplierid' => $subAccountSupplierid,
 
 
-                    ]);
-                }
-            }
+            ]);
+        }
 
-            if (Schema::hasTable('users')) {
-                $users=User::all();
-                View::share([
-                    'users'=>$users,
-                ]);
-            }
+        // $subAccountSupplierid=SubAccount::where('AccountClass',AccountClass::SUPPLIER->value)->get();
 
-            if (Schema::hasTable('user_permissions')) {
-                $us= auth()->id();
-                $use=UserPermission::where('User_id',$us)->get();
-                if(isset($use))
-                {
-                    View::share([
-                        'use'=>$use,
-                        'user'=>$us,
+        if(isset($subAccount)){
+            View::share([
+                'Warehouse'=>$subAccount,
 
-                    ]);
 
-                }
-            }
+            ]);
+        }
 
-            if (Schema::hasTable('business_data')) {
-                $buss=BusinessData::first();
-                View::share([
-                    'buss'=>$buss,
-                ]);
-            }
-
-            if (Schema::hasTable('currency_settings')) {
-                $cu=CurrencySetting::first();
-                View::share([
-                    'cu'=>$cu,
-                ]);
-            }
-
-            if (Schema::hasTable('default_customers')) {
-                $Default_customers=Default_customer::first();
-                View::share([
-                    'Default_customers'=>$Default_customers,
-                ]);
-            }
-
-            $transaction_typeExchangeBond="سند صرف";
-            $transaction_typePaymentBonds="سند قبض";
-
-            if (Schema::hasTable('payment_bonds')) {
-                $PaymentBonds= PaymentBond::where('transaction_type',$transaction_typePaymentBonds)->get();
-                $ExchangeBond= PaymentBond::where('transaction_type',$transaction_typeExchangeBond)->get();
-                View::share([
-                    'PaymentBonds'=>$PaymentBonds, 
-                    'ExchangeBond'=>$ExchangeBond,
-                ]);
-            }
-
-            if (Schema::hasTable('sub_accounts')) {
-                $SubAccounts=SubAccount::all();
-                View::share([
-                    'SubAccounts'=>$SubAccounts,
-                ]);
-            }
-
-            if (Schema::hasTable('main_accounts')) {
-                $MainAccounts= MainAccount::all();
-                View::share([
-                    'MainAccounts'=>$MainAccounts,
-                ]);
-            }
-
-            if (Schema::hasTable('currencies')) {
-                $Currencies=Currency::all();
-                View::share([
-                    'Currencies'=>$Currencies,
-                ]);
-            }
-
-            $accountClasses = AccountClass::cases();
-            $PaymentType=PaymentType::cases();
+        $accountClasses = AccountClass::cases();
+      $PaymentType=PaymentType::cases();
             $transactionTypes = TransactionType::cases();
             $AccountType = AccountType::cases();
+            View::share('transactionTypes', $transactionTypes);
+        $users=User::all();
+        View::share([
+            'accountClasses'=>$accountClasses,
+            'TypesAccounts'=>$TypesAccountName,
+            'AccountTypes'=>$AccountType,
+            'Deportattons'=>$dataDeportattons,
+            'PaymentType'=>$PaymentType,
+            'today '=> Carbon::now()->toDateString(),
+            'users'=>$users,
+        ]);
+        $Default_customers=Default_customer::first();
+        $us= auth()->id();
+        $use=UserPermission::where('User_id',$us)->get();
+        if(isset($use))
+        {
             View::share([
-                'accountClasses'=>$accountClasses,
-                'transactionTypes'=>$transactionTypes,
-                'AccountTypes'=>$AccountType,
-                'PaymentType'=>$PaymentType,
-                'today '=> Carbon::now()->toDateString(),
+                'use'=>$use,
+                'user'=>$us,
+
             ]);
 
-            if (Schema::hasTable('main_accounts')) {
-                $ASSETSAccountType_id=AccountType::FIXED_ASSETS;
-                $ASSETSAccountType2=Deportatton::CURRENT_ASSETS;
-                $MainAccount= MainAccount::where('typeAccount',$ASSETSAccountType_id)->get();
-
-                $LIABILITIes_OPPONENtsAccountType_id=AccountType::LIABILITIES_OPPONENTS;
-                $LIABILITIes_OPPONENtsAccountType_name=Deportatton::LIABILITIES_OPPONENTS;
-                $MainAccount2= MainAccount::where('typeAccount',$LIABILITIes_OPPONENtsAccountType_id)->get();
-
-                $EXPENSES_id=AccountType::EXPENSES;
-                $EXPENSES_name=Deportatton::EXPENSES;
-                $MainAccount3= MainAccount::where('typeAccount',$EXPENSES_id)->get();
-
-                $REVENUE_id=AccountType::REVENUE;
-                $REVENUE_name=Deportatton::REVENUE;
-
-                $MainAccount4= MainAccount::where('typeAccount',$REVENUE_id)->get();
-                $SubAccounts=SubAccount::all();
-
-                $currentDateTime = Carbon::now()->format('Y-m-d H:i:s');
-                View::share([
-                    'SubAccounts'=>$SubAccounts,
-                    'TypesAccounts'=>$TypesAccountName,
-                    'currentDateTime'=>$currentDateTime,
-
-                    'LIABILITIes_OPPONENtsAccountType_id'=>$LIABILITIes_OPPONENtsAccountType_id ,
-                    'LIABILITIes_OPPONENtsAccountType_name'=>$LIABILITIes_OPPONENtsAccountType_name ,
-                    'MainAccount2'=>$MainAccount2,
-
-
-
-                    'ASSETSAccountType2'=>$ASSETSAccountType2,
-                    'ASSETSAccountType_id'=>$ASSETSAccountType_id,
-                    'MainAccount'=>$MainAccount,
-
-
-
-                    'EXPENSES_name'=>$EXPENSES_name,
-                    'EXPENSES_id'=>$EXPENSES_id,
-                    'MainAccount3'=>$MainAccount3,
-
-                    'REVENUE_name'=>$REVENUE_name,
-                    'REVENUE_id'=>$REVENUE_id,
-                    'MainAccount4'=>$MainAccount4,
-
-
-
-
-                ]);
-            }
-
-        } catch (\Exception $e) {
-            // Log the error but don't prevent the application from booting
-            \Log::error('Error in AppServiceProvider boot: ' . $e->getMessage());
         }
+
+        // $cate=Category::all();
+        $buss=BusinessData::first();
+        $cu=CurrencySetting::first();
+$transaction_typeExchangeBond="سند صرف";
+$transaction_typePaymentBonds="سند قبض";
+if(isset($buss))
+        View::share([
+            'cu'=>$cu,
+            'buss'=>$buss,
+            'Default_customers'=>$Default_customers,
+            $PaymentBonds= PaymentBond::where('transaction_type',$transaction_typePaymentBonds)->get(),
+            $ExchangeBond= PaymentBond::where('transaction_type',$transaction_typeExchangeBond)->get(),
+
+            $SubAccounts=SubAccount::all(),
+           $MainAccounts= MainAccount::all(),
+           $Currencies=Currency::all(),
+           'PaymentBonds'=>$PaymentBonds, 'ExchangeBond'=>$ExchangeBond,'SubAccounts'=>$SubAccounts,'MainAccounts'=>$MainAccounts,'Currencies'=>$Currencies,
+
+        ]);
+        if(isset($buss)){
+
+$ASSETSAccountType_id=AccountType::FIXED_ASSETS;
+ $ASSETSAccountType2=Deportatton::CURRENT_ASSETS;
+ $MainAccount= MainAccount::where('typeAccount',$ASSETSAccountType_id)->get();
+
+ $LIABILITIes_OPPONENtsAccountType_id=AccountType::LIABILITIES_OPPONENTS;
+ $LIABILITIes_OPPONENtsAccountType_name=Deportatton::LIABILITIES_OPPONENTS;
+ $MainAccount2= MainAccount::where('typeAccount',$LIABILITIes_OPPONENtsAccountType_id)->get();
+
+ $EXPENSES_id=AccountType::EXPENSES;
+ $EXPENSES_name=Deportatton::EXPENSES;
+ $MainAccount3= MainAccount::where('typeAccount',$EXPENSES_id)->get();
+
+ $REVENUE_id=AccountType::REVENUE;
+ $REVENUE_name=Deportatton::REVENUE;
+
+ $MainAccount4= MainAccount::where('typeAccount',$REVENUE_id)->get();
+ $SubAccounts=SubAccount::all();
+
+//  $SubAccounts= SubAccount::where('Main_id',$ $MainAccount->main_account_id)->get();
+$currentDateTime = Carbon::now()->format('Y-m-d H:i:s');
+ View::share([
+    'SubAccounts'=>$SubAccounts,
+    'TypesAccounts'=>$TypesAccountName,
+    'currentDateTime'=>$currentDateTime,
+
+       'LIABILITIes_OPPONENtsAccountType_id'=>$LIABILITIes_OPPONENtsAccountType_id ,
+       'LIABILITIes_OPPONENtsAccountType_name'=>$LIABILITIes_OPPONENtsAccountType_name ,
+       'MainAccount2'=>$MainAccount2,
+
+
+
+         'ASSETSAccountType2'=>$ASSETSAccountType2,
+         'ASSETSAccountType_id'=>$ASSETSAccountType_id,
+         'MainAccount'=>$MainAccount,
+
+
+
+         'EXPENSES_name'=>$EXPENSES_name,
+         'EXPENSES_id'=>$EXPENSES_id,
+         'MainAccount3'=>$MainAccount3,
+
+         'REVENUE_name'=>$REVENUE_name,
+         'REVENUE_id'=>$REVENUE_id,
+         'MainAccount4'=>$MainAccount4,
+
+
+
+
+    ]);
+
+}
     }
 }
+
