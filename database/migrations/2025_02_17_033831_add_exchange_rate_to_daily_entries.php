a55cc56c061f9e5e2fb0196dc6bd6be33c255c4f
Fadl711 <fathlvvee@gmail.com>
2025-02-21 00:10:00 +0300

    faf

--- database/migrations/2025_02_17_033831_add_exchange_rate_to_daily_entries.php
+++ database/migrations/2025_02_17_033831_add_exchange_rate_to_daily_entries.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\DailyEntrie;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,7 +19,7 @@ return new class extends Migration
            if ($pb) {
         foreach ($pb as $p)
          {
-            
+
 
                 $p->Currency_name = "ريال.يمني";
                 $p->save();
