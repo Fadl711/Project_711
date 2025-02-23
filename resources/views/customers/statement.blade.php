55cb2ca9f326fb28bf4c798414da219428f578b7
jamalAli-711 <fathlvvrr@gmail.com>
2025-02-23 20:08:23 +0300

    Currency_name1995

--- resources/views/customers/statement.blade.php
+++ resources/views/customers/statement.blade.php
@@ -5,14 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> كشف حساب {{$Myanalysis}}</title>
-<<<<<<< HEAD
     <link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-=======
 
     
->>>>>>> ef838eb9b09c2a6486000d6589f9650bd51e3257
     <link href="{{ asset('css/tailwind.css') }}" rel="stylesheet">
     <!-- jQuery -->
     <script src="{{ asset('assets/js/jquery/dist/jquery.min.js') }}"></script>
@@ -211,17 +208,14 @@
                             <td class=" text-right "    style="width: 300px; {{ $cellColor }}">{{ $entrie->Statement }}</td>
                             <td class=" text-center " style="width: 90px; ">{{ $entrie->entrie_id }}</td>
                             <td class="text-right px-1" style="width: 120px; {{ $cellColor }}">
-<<<<<<< HEAD
                                 {{ $entrie->total_debit != 0 ? number_format($entrie->total_debit, 0, '.', ',') : '' }}
                             </td>         
                                                <td class="text-right px-1"  style="width: 120px; " >
                                                 {{ $entrie->total_credit != 0 ? number_format($entrie->total_credit, 0, '.', ',') : '' }}
                                             </td>
-=======
                                 {{ number_format($entrie->total_debit ?? 0) }}
                             </td>
                                                <td class="text-right px-1"  style="width: 120px; " >{{ number_format( $entrie->total_credit ?? 0) }}</td>
->>>>>>> ef838eb9b09c2a6486000d6589f9650bd51e3257
 
                                                 <td class=" text-center " style="width: 100px; ">{{ $entrie->Currency_name ?? ''}}</td>
                         </tr>
@@ -392,13 +386,10 @@
             <button id="myButton" class="px-4 py-2 bgcolor text-white rounded-lg shadow-md hover:bg-blue-600">
         تغيير الألوان
             </button>
-<<<<<<< HEAD
            
             <button id="myButton2" class="px-4 py-2 bgcolor2 text-black rounded-lg bg-amber-400 shadow-md hover:bg-blue-600">
-=======
 
             <button id="myButton2" class="px-4 py-2 bgcolor2 text-black rounded-lg shadow-md hover:bg-blue-600">
->>>>>>> ef838eb9b09c2a6486000d6589f9650bd51e3257
         تغيير الألوان
             </button>
             <button id="myButton3" class="px-4 py-2 bgcolor3  rounded-lg shadow-md hover:bg-blue-600">
