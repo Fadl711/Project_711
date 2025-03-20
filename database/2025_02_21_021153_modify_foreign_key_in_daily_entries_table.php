2b5c313f2c659c745fe61a28190decf5cbdfe0bb
Fadl711 <fathlvvee@gmail.com>
2025-03-20 23:41:28 +0300

    faf

--- database/2025_02_21_021153_modify_foreign_key_in_daily_entries_table.php
+++ database/2025_02_21_021153_modify_foreign_key_in_daily_entries_table.php
@@ -8,10 +8,10 @@ return new class extends Migration {
     {
         Schema::table('daily_entries', function (Blueprint $table) {
             $table->dropForeign(['account_Credit_id']); // حذف المفتاح الأجنبي القديم
-/*             $table->foreign('account_Credit_id')
+            $table->foreign('account_Credit_id')
                   ->references('sub_account_id')
                   ->on('sub_accounts')
-                  ->onDelete('restrict'); // منع الحذف */
+                  ->onDelete('restrict'); // منع الحذف
         });
     }
 
