6d04370ab8b8ec8466383a3d613e6016caa43d87
jamalAli-711 <fathlvvrr@gmail.com>
2025-03-24 01:11:32 +0300

    sync3

--- config/database.php
+++ config/database.php
@@ -85,19 +85,20 @@ return [
 
      'pgsql' => [
     'driver' => 'pgsql',
-    'host' => env('DB_HOST_PGSQL', ''),
-    'port' => env('DB_PORT_PGSQL', ''),
-    'database' => env('DB_DATABASE_PGSQL', ''),
-    'username' => env('DB_USERNAME_PGSQL', ''),
-    'password' => env('DB_PASSWORD_PGSQL', ''),
-    'charset' => 'utf8',
-    'prefix' => '',
-    'schema' => 'public',
-    'options' => [
-        PDO::ATTR_EMULATE_PREPARES => true,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        // هنا يمكنك إضافة خيارات إضافية إذا لزم الأمر
-    ],
+    'url' => env('DB_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'fadldatabases'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+   
 ],
 
         'sqlsrv' => [
